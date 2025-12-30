5d8ea44ef8bdd58662312f554e35abfd1485bf72
Salah <mosalahkd028@gmail.com>
2025-12-30 03:58:26 -0700

    Update session authorization middleware by jwt

--- backend/middlewares/userAuth.js
+++ backend/middlewares/userAuth.js
@@ -1,7 +1,20 @@
 // middleware to check if user is authenticated
+
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 const userAuth = (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    return res.status(401).json({ success: false, message: "please log in." });
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.json({ success: false, message: "please log in." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { _id: decoded.id };
+  } catch (error) {
+    return res.json({ success: false, message: "invalid token." });
   }
 
   next();
